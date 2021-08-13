906e218497632cd881d052a7c7169c8f790678d1
lechitu-24h <lechitu-24h@users.noreply.github.com>
2021-08-13 04:23:01 +0000

    deploy: 1dd8c1673a26fe10f13f97177e6da5da4a2f7237

---
+++ precache-manifest.dfa423b06921fdfd84372992f4d052d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6e7587452710b682b4671648674c22",
+    "url": "https://lechitu-24h.githup.io/proj-task-management-fe/index.html"
+  },
+  {
+    "revision": "662f1b8ff53463bb6e63",
+    "url": "https://lechitu-24h.githup.io/proj-task-management-fe/static/css/main.1b2d26a0.chunk.css"
+  },
+  {
+    "revision": "fee080c770163fe05abc",
+    "url": "https://lechitu-24h.githup.io/proj-task-management-fe/static/js/2.0080eb52.chunk.js"
+  },
+  {
+    "revision": "662f1b8ff53463bb6e63",
+    "url": "https://lechitu-24h.githup.io/proj-task-management-fe/static/js/main.7394f299.chunk.js"
+  },
+  {
+    "revision": "98511fe6a52625da9cd6",
+    "url": "https://lechitu-24h.githup.io/proj-task-management-fe/static/js/runtime~main.ce349cb2.js"
+  }
+]);
